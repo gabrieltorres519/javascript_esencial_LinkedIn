73b59e581c9d0aa75e5c0ce6872ae8fd5a30f680
unknown <gabrieltorres9909@gmail.com>
2023-06-22 19:28:22 -0600

    Usando modo estricto para forzar la declaración de variables y evitar el uso de palabras reservadas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-alert('Hola desde un archivo externo')
+"use strict"
 
+// alert('Hola desde un archivo externo')
+// Comentario
 console.log('Hola consola')
+
+var x;
+
+x = "3.1416"
+
+var no_public = 'Hola'
