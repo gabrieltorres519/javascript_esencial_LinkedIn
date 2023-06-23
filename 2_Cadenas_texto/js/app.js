128eb2bb5cded991eec2d2ff5b98cb1fdf9344cf
unknown <gabrieltorres9909@gmail.com>
2023-06-23 13:27:51 -0600

    Trabajo básico con strings

---
+++ 2_Cadenas_texto/js/app.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// El uso de comillas simples o dobles es indistinto al crear una variable tipo string 
+
+var bebida = "agua";
+
+var comida = 'ceviche';
+
+// Anidando tipos diferentes de comillas 
+
+var instruccion = "El platillo se llama 'Ceviche' ";
+
+// Conversión de números a cadenas de texto
+
+var edad = 23;
+
+var edadtxt = String(edad)
+
