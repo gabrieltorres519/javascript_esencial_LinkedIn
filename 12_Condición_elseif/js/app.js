584d3a7c96f11140a1ad5c78099ac016c20eb023
unknown <gabrieltorres9909@gmail.com>
2023-06-23 19:16:04 -0600

    elseif

---
+++ 12_Condición_elseif/js/app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var datoA = 10;
+var datoB = 20;
+var resultado = "Sin datos";
+
+if(datoA > datoB){
+    resultado = 'La condición se cumplió';
+}else if(datoB === datoA){
+    resultado = 'Segunda condición valida'
+}else{
+    resultado = 'La condición no se cumplió';
+}
