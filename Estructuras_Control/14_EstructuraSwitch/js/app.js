7c785334b7ea95cd33521117618b77485bf3067e
unknown <gabrieltorres9909@gmail.com>
2023-06-23 20:28:01 -0600

    Reestructurando y agregando estructura switch y ciclo for

---
+++ Estructuras_Control/14_EstructuraSwitch/js/app.js
@@ -0,0 +1,19 @@
+"use strict"
+
+var edad = 23;
+var resultado = '';
+
+switch (edad){
+    case 23:
+        resultado = 'Hola tienes 23 años'
+        console.log(resultado)
+        break;
+    
+    case 15:
+        resultado = 'Hola tienes 15 años'
+        console.log(resultado)
+        break;
+
+    default:
+        break;
+}
---
+++ Estructuras_Control/15_Ciclo_for/js/app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var productos = 5;
+
+// Mostrar cada vez que iteramos 
+
+for(let contador = 0; contador < productos; contador++){
+    console.log("Producto # "+contador);
+    debugger;
+}
