c4057602e9e084fca6decc7623603fc9118c54e1
unknown <gabrieltorres9909@gmail.com>
2023-06-23 18:26:12 -0600

    Operadores relacionales y lógicos

---
+++ 8_Operadores_Relacionales_JS/js/app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Los operadores aritméticos básicos son los mismos que en cualquier lenguaje de programación
+// solo el 'igual que' es un pcoco distinto.
+
+var num1 = 5;
+var num2 = '5';
+
+var result1 = num1 === num2;
+var result2 = num1 == num2; 
---
+++ 9_Operadores_Lógicos/js/app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var datoA = 10;
+var datoB = 20;
+
+// AND - &&
+var and = (datoA > 10 && datoB > 10);
+
+// OR - || (doble pipe)
+var or = (datoA > 10 || datoB > 10);
+
+// not - !
+var not = !(datoA > 10);
