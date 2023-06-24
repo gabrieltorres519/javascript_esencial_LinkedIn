89a26ae46cf7020c0f4390839d72b716c5554ba4
unknown <gabrieltorres9909@gmail.com>
2023-06-24 16:25:28 -0600

    para recibir el array usando una coma en lugar de concatenar

--- Estructuras_Control/21_ParametrosREST/js/app.js
+++ Estructuras_Control/21_ParametrosREST/js/app.js
@@ -5,7 +5,7 @@
 function cocinar(ingrediente1, ingrediente2, ...restoIngredientes){
     console.log('Ingrediente 1: '+ingrediente1);
     console.log('Ingrediente 2: '+ingrediente2);
-    console.log('Resto de ingredientes: '+restoIngredientes);
+    console.log('Resto de ingredientes: ',restoIngredientes);
 }
 
 cocinar('Pollo','Tomate','Arroz','Frijoles','Pescado')
