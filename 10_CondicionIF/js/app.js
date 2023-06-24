a35cc125888754fd3e04694d58233ae86eb58796
unknown <gabrieltorres9909@gmail.com>
2023-06-23 19:09:22 -0600

    Condiciones If e If-Else

---
+++ 10_CondicionIF/js/app.js
@@ -0,0 +1,9 @@
+"use strict"
+
+var datoA = 10;
+var datoB = 20;
+var resultado = "Sin datos";
+
+if(datoA > datoB){
+    resultado = 'La condición se cumplió';
+}
---
+++ 11_CondicionIfElse/js/app.js
@@ -0,0 +1,11 @@
+"use strict"
+
+var datoA = 10;
+var datoB = 20;
+var resultado = "Sin datos";
+
+if(datoA > datoB){
+    resultado = 'La condición se cumplió';
+}else{
+    resultado = 'La condición no se cumplió';
+}
