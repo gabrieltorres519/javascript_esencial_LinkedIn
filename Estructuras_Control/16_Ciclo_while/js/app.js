ff6a3ab5cff3dee9acf526ffb1329eae0940cc5a
unknown <gabrieltorres9909@gmail.com>
2023-06-23 20:45:16 -0600

    Ciclos while y do-while

---
+++ Estructuras_Control/16_Ciclo_while/js/app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var productos = 5;
+
+
+while(productos > 0){
+    console.log(productos)
+    productos--;
+    debugger;
+}
---
+++ Estructuras_Control/17_Ciclo_Do_While/js/app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var productos = 5;
+
+
+do{
+    console.log(productos)
+    productos--;
+    debugger;
+}while(productos > 0);
