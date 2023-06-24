f1a08c83dea4b7231b3f9f15c3ed6627d29345d9
unknown <gabrieltorres9909@gmail.com>
2023-06-24 17:31:18 -0600

    Modificación del código que utiliza SPREAD

--- Estructuras_Control/22_ParámetrosTipoSPREAD/js/app.js
+++ Estructuras_Control/22_ParámetrosTipoSPREAD/js/app.js
@@ -2,13 +2,14 @@
 
 // Parámetros SPREAD
 
-function cocinar(ingrediente1, ingrediente2, ingrediente3){
+function cocinar(ingrediente1, ingrediente2, ingrediente3, ...otros){
     console.log('Ingrediente 1: ',ingrediente1);
     console.log('Ingrediente 2: ',ingrediente2);
     console.log('Ingrediente 3: ',ingrediente3);
+    console.log('otros: ',otros);
 
 }
 
 var ingredientesBase = ["Pollo","Tomate"]
 
-cocinar(...ingredientesBase, "Arroz")
+cocinar(...ingredientesBase, "Arroz", "Pescado", "Chile")
