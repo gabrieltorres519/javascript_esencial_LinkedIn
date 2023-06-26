76e050a8138bc03f3aecddf8d35c2d13d9915fa9
unknown <gabrieltorres9909@gmail.com>
2023-06-26 17:18:03 -0600

    Evento de carga completada del documento

---
+++ DOM_Eventos_Num_Arrays/29_Evento_CargadelDocumento/js/app.js
@@ -0,0 +1,13 @@
+ "use strict"
+
+// Eventos de teclado
+
+// keydown 
+// keypress
+// keyup
+
+window.addEventListener('load', function(){
+    console.log('El contenido de la ventana se ha cargado');
+});
+
+
