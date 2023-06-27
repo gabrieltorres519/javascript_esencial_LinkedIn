b263619cf58ca1eff76e531022f5c3971c35d9f4
unknown <gabrieltorres9909@gmail.com>
2023-06-26 19:05:24 -0600

    Uso de setTimeout() para un ciclo infinito con instrucciones que se repiten en un intervalo de tiempo

---
+++ DOM_Eventos_Num_Arrays/31_Temporizadores/js/app.js
@@ -0,0 +1,16 @@
+ "use strict"
+
+var temporizador = setInterval(function (){
+    setColor()
+}, 2000)
+
+function setColor(){
+    var pagina = document.body;
+    pagina.style.backgroundColor = pagina.style.backgroundColor == 'blue' ? "green" : "blue"; 
+}
+
+function stopChangeColor(){
+    clearInterval(temporizador)
+}
+
+
