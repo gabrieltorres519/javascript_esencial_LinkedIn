cd9923bcad00fa399adfc2e6e5f88f01b099bce8
unknown <gabrieltorres9909@gmail.com>
2023-06-28 21:24:26 -0600

    Métodos del tipo numérico

---
+++ DOM_Eventos_Num_Arrays/36_MetodosNumericosJS/js/app.js
@@ -0,0 +1,15 @@
+ "use strict"
+
+ // Métodos numéricos
+
+ var numero = "23"
+
+ console.log("Number: ", typeof numero, typeof Number(numero));
+ console.log("parseInt: ", parseInt(numero));
+ console.log("parseFloat: ", Number.parseFloat(numero));
+ console.log("isNaN: ", isNaN(numero)); //Identifica si el texto del string que se le ingresa no es un número
+ console.log("isInteger: ", Number.isInteger(numero));
+
+ 
+
+
